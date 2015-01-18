a2a6e5fc18ec234b8040d6ee97b264823ca6fa7a
Simão Mata <me@simao.io>
2015-01-18 17:10:11 +0100

    simple test version for sensor_py

---
+++ sensor_py/send.py
@@ -0,0 +1,29 @@
+
+def send_message(sock, message):
+    """ Send a serialized message (protobuf Message interface)
+    to a socket, prepended by its length packed in 4
+    bytes (big endian).
+    """
+    s = message.SerializeToString()
+    packed_len = struct.pack('>L', len(s))
+    sock.sendall(packed_len + s)
+    
+
+import sys
+
+sys.path.append('.')
+
+import socket 
+import messages_pb2
+import struct
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect(("127.0.0.1", 6767))
+
+m = messages_pb2.Measurement()
+m.value = 22.0
+m.time = "NOW"
+
+send_message(socket, m)
+
+socket.close()

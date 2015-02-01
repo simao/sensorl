b3d593c0886b55e69c3f8d2fb7d925cd86a0afb6
Simão Mata <me@simao.io>
2015-02-01 08:02:20 +0100

    try to use rrd tool from extension

--- src/main/scala/io/simao/sensorl/Sensorl.scala
+++ src/main/scala/io/simao/sensorl/Sensorl.scala
@@ -2,21 +2,14 @@ package io.simao.sensorl
 
 import io.simao.librrd.LibRRD
 import io.simao.sensorl.db.MeasurementDatabase
-import io.simao.sensorl.server.{RRDToolReceiver, DatabaseReceiver, Server}
+import io.simao.sensorl.server.{DatabaseReceiver, Server}
 
 
 object Sensorl extends App {
   val dbReceiverFn = (_: Unit) ⇒ {
-    new DatabaseReceiver(MeasurementDatabase.withConnection("jdbc:sqlite:measurements.db"))
+    new DatabaseReceiver(MeasurementDatabase
+      .withConnection("/home/simao/code/sensorl/lol.rrd"))
   }
-
-  val rrdArgs = Array(
-    "/home/simao/code/sensorl/lol.rrd",
-    "--start", "920804400",
-    "DS:speed:COUNTER:600:U:U",
-    "RRA:AVERAGE:0.5:1:24",
-    "RRA:AVERAGE:0.5:6:10"
-  )
 
   val updates = Array(
     "/home/simao/code/sensorl/lol.rrd",
@@ -28,9 +21,7 @@ object Sensorl extends App {
   println(LibRRD.rrdcreate(rrdArgs))
   println(LibRRD.rrdupdate(updates))
 
-  val rrdToolReceiverFn = (_: Unit) ⇒ { new RRDToolReceiver }
-
-  val server = new Server(6767, rrdToolReceiverFn)
+  val server = new Server(6767, dbReceiverFn)
 
   server.startServer()
 }
--- src/main/scala/io/simao/sensorl/db/MeasurementDatabase.scala
+++ src/main/scala/io/simao/sensorl/db/MeasurementDatabase.scala
@@ -1,40 +1,46 @@
 package io.simao.sensorl.db
 
-
-import java.sql.{Connection, DriverManager}
+import java.io.File
+import com.typesafe.scalalogging.LazyLogging
+import io.simao.librrd.LibRRD
 import io.simao.sensorl.message.Measurement
 import io.simao.util.KestrelObj._
-import org.joda.time.DateTime
+
 
 object MeasurementDatabase {
-  def apply(jdbcString: String) = {
-    val connection = DriverManager.getConnection(jdbcString)
-    new MeasurementDatabase(connection)
+  def apply(fileName: String) = {
+    val file = new File(fileName).getAbsolutePath
+    new MeasurementDatabase(file)
   }
 
-  def withConnection[T](jdbcString: String)(f: MeasurementDatabase ⇒ T) = {
-    val connection = DriverManager.getConnection(jdbcString)
-    val db = new MeasurementDatabase(connection)
+  def withConnection[T](fileName: String)(f: MeasurementDatabase ⇒ T) = {
+    val db = MeasurementDatabase(fileName)
     try f(db)
-    finally connection.close()
+    finally {}
   }
 }
 
-class MeasurementDatabase(connection: Connection) {
+class MeasurementDatabase(fileName: String) extends LazyLogging {
   def save(item: Measurement): Measurement = {
     item.tap { i ⇒
-      val statement = connection.createStatement()
-      val now = DateTime.now().toString
-      statement.executeUpdate(s"insert into measurements values('${item.mid}', '${item.value}', '${item.time}')")
+      val args = Array(fileName, s"N:${item.value}")
+      LibRRD.rrdupdate(args)
     }
   }
 
-  def setupTables(drop: Boolean = false): Connection = {
-    connection.tap { c ⇒
-      val statement = c.createStatement()
-      if (drop)
-        statement.executeUpdate("drop table if exists measurements")
-      statement.executeUpdate("create table if not exists measurements (mid integer, value float, updated_at string)")
+  def setupDb(drop: Boolean): Unit = {
+    if (drop) {
+      if(new File(fileName).delete())
+        logger.info("Database {} deleted", fileName)
     }
+
+    val rrdArgs = Array(
+      fileName,
+      "DS:speed:COUNTER:600:U:U",
+      "RRA:AVERAGE:0.5:1:24",
+      "RRA:AVERAGE:0.5:6:10"
+    )
+
+    LibRRD.rrdcreate(Array(fileName))
   }
 }

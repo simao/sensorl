3d5ca23eb584040309b85f7d6a9b55a7e82f843d
Simão Mata (RPI) <simao.m@gmail.com>
2015-02-03 06:02:10 +0100

    py_sensor working on raspberry pi

--- build.sbt
+++ build.sbt
@@ -18,4 +18,3 @@ libraryDependencies += "joda-time" % "joda-time" % "2.6"
 
 libraryDependencies += "org.joda" % "joda-convert" % "1.7"
 
-libraryDependencies += "org.xerial" % "sqlite-jdbc" % "3.8.7"
--- src/main/scala/io/simao/sensorl/Sensorl.scala
+++ src/main/scala/io/simao/sensorl/Sensorl.scala
@@ -4,13 +4,13 @@ import io.simao.sensorl.db.MeasurementDatabase
 import io.simao.sensorl.server.{DatabaseReceiver, Server}
 
 object Sensorl extends App {
-  val dbFilename = "/home/simao/code/sensorl/temp.rrd"
+  val dbFilename = "temp.rrd"
   
   val dbReceiverFn = (_: Unit) ⇒ {
     new DatabaseReceiver(MeasurementDatabase(dbFilename))
   }
 
-  // MeasurementDatabase(dbFilename).setupDb(true)
+  MeasurementDatabase(dbFilename).setupDb(false)
   
   val server = new Server(6767, dbReceiverFn)
 

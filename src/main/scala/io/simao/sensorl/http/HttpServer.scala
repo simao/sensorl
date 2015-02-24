008b70f88615656c414108b1873ab1272449e3cd
Simão Mata <me@simao.io>
2015-02-24 18:28:19 +0100

    added dummy jetty servlet

--- src/main/scala/io/simao/sensorl/http/HttpServer.scala
+++ src/main/scala/io/simao/sensorl/http/HttpServer.scala
@@ -1,13 +1,36 @@
 package io.simao.sensorl.http
 
 import java.io.File
+import javax.servlet.http.{HttpServletResponse, HttpServletRequest, HttpServlet}
 
 import com.typesafe.scalalogging.LazyLogging
+import io.simao.sensorl.db.MeasurementDatabase
+
 import org.eclipse.jetty.server.{Handler, Server}
 import org.eclipse.jetty.server.handler.{DefaultHandler, HandlerList, ResourceHandler}
+import org.eclipse.jetty.servlet.ServletHandler
+import org.joda.time.DateTime
 
 import scala.concurrent.{ExecutionContext, Future}
 
+class MetricsApiServlet extends HttpServlet {
+
+  // TODO: How to inject this dependency
+  def db(): MeasurementDatabase = {
+    MeasurementDatabase("temp.rrd")
+  }
+
+  def values(): List[(Long, Double)] = {
+    db().fetchValues(new DateTime(), "temp")
+  }
+
+  override def doGet(req: HttpServletRequest, resp: HttpServletResponse): Unit = {
+    resp.setContentType("text/html")
+    resp.setStatus(HttpServletResponse.SC_OK)
+    resp.getWriter().println("<h1>Hello from HelloServlet</h1>")
+    resp.getWriter().println(values().mkString("<br>\n"))
+  }
+}
 
 class HttpServer(val dir: File, val port: Int = 8080) extends LazyLogging {
   def start()(implicit ec: ExecutionContext) = {
@@ -18,8 +41,12 @@ class HttpServer(val dir: File, val port: Int = 8080) extends LazyLogging {
     resource_handler.setWelcomeFiles(Array[String]("index.html"))
     resource_handler.setResourceBase(dir.getAbsolutePath)
 
+    val handler = new ServletHandler()
+    handler.addServletWithMapping(classOf[MetricsApiServlet], "/api/*")
+
     val handlers = new HandlerList()
-    handlers.setHandlers(Array[Handler](resource_handler, new DefaultHandler()))
+    handlers.setHandlers(Array[Handler](resource_handler, handler, new DefaultHandler()))
+
     server.setHandler(handlers)
 
     logger.info("Starting http server on port {} serving {}", port.toString, dir.toString)

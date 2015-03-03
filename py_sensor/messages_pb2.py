d74b44fabbb15a826982dcdaf60e667703fefba5
Simão Mata <me@simao.io>
2015-03-03 17:05:03 +0100

    send a key with a measurement

--- py_sensor/messages_pb2.py
+++ py_sensor/messages_pb2.py
@@ -12,36 +12,43 @@ from google.protobuf import descriptor_pb2
 
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='messages.proto',
-  package='',
-  serialized_pb='\n\x0emessages.proto\"7\n\x0bMeasurement\x12\x0b\n\x03mid\x18\x01 \x02(\r\x12\r\n\x05value\x18\x02 \x02(\x01\x12\x0c\n\x04time\x18\x03 \x02(\t')
+  package='io.simao.sensorl.message',
+  serialized_pb='\n\x0emessages.proto\x12\x18io.simao.sensorl.message\"D\n\x0bMeasurement\x12\x0b\n\x03mid\x18\x01 \x02(\r\x12\r\n\x05value\x18\x02 \x02(\x01\x12\x0c\n\x04time\x18\x03 \x02(\t\x12\x0b\n\x03key\x18\x04 \x02(\t')
 
 
 
 
 _MEASUREMENT = _descriptor.Descriptor(
   name='Measurement',
-  full_name='Measurement',
+  full_name='io.simao.sensorl.message.Measurement',
   filename=None,
   file=DESCRIPTOR,
   containing_type=None,
   fields=[
     _descriptor.FieldDescriptor(
-      name='mid', full_name='Measurement.mid', index=0,
+      name='mid', full_name='io.simao.sensorl.message.Measurement.mid', index=0,
       number=1, type=13, cpp_type=3, label=2,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='value', full_name='Measurement.value', index=1,
+      name='value', full_name='io.simao.sensorl.message.Measurement.value', index=1,
       number=2, type=1, cpp_type=5, label=2,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='time', full_name='Measurement.time', index=2,
+      name='time', full_name='io.simao.sensorl.message.Measurement.time', index=2,
       number=3, type=9, cpp_type=9, label=2,
+      has_default_value=False, default_value=unicode("", "utf-8"),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='key', full_name='io.simao.sensorl.message.Measurement.key', index=3,
+      number=4, type=9, cpp_type=9, label=2,
       has_default_value=False, default_value=unicode("", "utf-8"),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -55,8 +62,8 @@ _MEASUREMENT = _descriptor.Descriptor(
   options=None,
   is_extendable=False,
   extension_ranges=[],
-  serialized_start=18,
-  serialized_end=73,
+  serialized_start=44,
+  serialized_end=112,
 )
 
 DESCRIPTOR.message_types_by_name['Measurement'] = _MEASUREMENT
@@ -65,7 +72,7 @@ class Measurement(_message.Message):
   __metaclass__ = _reflection.GeneratedProtocolMessageType
   DESCRIPTOR = _MEASUREMENT
 
-  # @@protoc_insertion_point(class_scope:Measurement)
+  # @@protoc_insertion_point(class_scope:io.simao.sensorl.message.Measurement)
 
 
 # @@protoc_insertion_point(module_scope)
--- py_sensor/send.py
+++ py_sensor/send.py
@@ -1,19 +1,13 @@
-#!/usr/bin/python
-# Can enable debug output by uncommenting:
-#import logging
-#logging.basicConfig(level=logging.DEBUG)
 
-import time
-import Adafruit_MCP9808.MCP9808 as MCP9808
 import sys
 sys.path.append('.')
-import socket
+
+import time
+import socket 
 import messages_pb2
 import struct
+import random
 from datetime import datetime
-
-sensor = MCP9808.MCP9808()
-sensor.begin()
 
 def send_message(sock, message):
     """ Send a serialized message (protobuf Message interface)
@@ -25,23 +19,18 @@ def send_message(sock, message):
     sock.sendall(packed_len)
     sock.sendall(s)
 
-def build_msg(temp):
+def build_msg():
     m = messages_pb2.Measurement()
-    m.mid = 0
-    m.value = temp
+    m.mid = 1
+    m.value = random.randint(-10, 40)
     m.time = datetime.utcnow().isoformat()
-    m.key = "temp"
     return m
 
-print sys.argv
-
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect((sys.argv[1], int(sys.argv[2])))
+socket.connect(("127.0.0.1", 6767))
 
 while True:
-    temp = sensor.readTempC()
-    send_message(socket, build_msg(temp))
-    print 'Temperature: {0:0.3F}*C'.format(temp)
-    time.sleep(5.0)
+    send_message(socket, build_msg())
+    time.sleep(2)
 
 socket.close()
